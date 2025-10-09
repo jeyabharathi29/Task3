010492e0b7df375308f73518c481c4301f9af175
Jeya Bharathi <jeyabharathi2903@gmail.com>
2025-10-09 17:42:01 +0530

    Delete fuzz.py

--- fuzz.py
+++
@@ -1,9 +0,0 @@
-number =int(input("Enter a number:"))
-if(number%3==0)and(number%5==0):
-    print("FUZZ and BUZZ")
-elif(number%5==0):
-    print("BUZZ")
-elif(number%3==0):
-    print("FUZZ")
-else:
-    print("statement is not clear")
