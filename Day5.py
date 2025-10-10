807ff935ac2b606691e67415f9aef50e061c8f49
Jeya Bharathi <jeyabharathi2903@gmail.com>
2025-10-10 16:57:07 +0530

    Day5

---
+++ Day5.py
@@ -0,0 +1,37 @@
+#Task1
+for i in range(10,0,-1):
+    print(i)
+
+#Task2
+n=0
+while n<5:
+    n+=1
+    print("Jeyabharathi")
+
+
+#Task3
+print("Task3")
+n=0
+while n<10:
+    n+=1
+    print("Jeyabharathi")
+
+#Task4
+print("Task4")
+n=0
+while n<10:
+    n+=1
+    print("Jeyabharathi")
+    if(n==5):
+        break;
+
+#Task5
+f=1
+num=int(input("Enter your number:"))
+for num in range(1,num+1):
+    f=f*num
+print("Factorial is",f)
+
+
+    
+    
