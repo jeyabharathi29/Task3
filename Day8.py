c15ba8997a0458fef63adfe06d60b86941509763
Jeya Bharathi <jeyabharathi2903@gmail.com>
2025-10-15 17:05:25 +0530

    Day8

---
+++ Day8.py
@@ -0,0 +1,28 @@
+#Task1
+list=[]
+sum=0
+n=int(input("Enter your number:"))
+while n>0:
+    sum+=n
+    n=int(input("Enter your number:"))
+print(sum)
+
+#Task2
+print("Task2")
+list=[1,4,-5,-8,9]
+i=0
+p1=[]
+p2=[]
+for i in list:
+    if i>0:
+        p1.append(i)
+    else:
+        p2.append(i)
+print(p1)
+print(p2)
+    
+    
+
+
+    
+    
